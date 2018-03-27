409a90d0b87ca016359942cb6fbc655b158de98e
Daryoush <daryoush_farsimadan@hotmail.com>
2018-03-27 15:02:05 +0300

    Target switch

--- js/PieApp.js
+++ js/PieApp.js
@@ -1,11 +1,13 @@
 
-function RenderPie(paramxy) {
+function RenderPie(data) {
     //Kutsutaan komentoa tukiluokasta ja tallennetaan piirakan renderöintiin käytettävät arvot arraylistaan.
-    console.log(paramxy);
     ClearCanvas();
-    var catchdata = returndata();
-    catchdata = catchdata[0].rootTarget.views.filter((ob) => ob.view == paramxy);
-    var data = catchdata[0].targets;
+    //var catchdata = returndata();
+    //catchdata = catchdata[0].rootTarget.views.filter((ob) => ob.view == paramxy);
+    //var data = catchdata[0].targets;
+
+
+
     console.log(data);
 
     var width = 960,
@@ -21,8 +23,8 @@ function RenderPie(paramxy) {
         .innerRadius(0);
 
     var labelArc = d3.arc()
-        .outerRadius(radius - 40)
-        .innerRadius(radius - 40);
+        .outerRadius(radius - 10)
+        .innerRadius(radius - 70);
 
     var pie = d3.pie()
         .sort(null)
@@ -35,9 +37,10 @@ function RenderPie(paramxy) {
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var g = svg.selectAll(".arc")
-        .data(pie(data))
+        .data(pie(data[0].targets))
         .enter().append("g")
-        .attr("class", "arc");
+        .attr("class", "arc")
+        .attr ("id",function(d){return d.data.target});
 
     g.append("path")
         .attr("d", arc)
@@ -48,6 +51,12 @@ function RenderPie(paramxy) {
         .attr("dy", ".35em")
         .text(function (d) { return d.data.target + " " + d.data.marketValue; });
 
+    d3.selectAll("g")
+        .filter(".arc")
+        .on("click", function (){
+        console.log(d3.select(this).attr("id"));
+       RenderPie(ReturnFilteredView(data[0].view, d3.select(this).attr("id")));
+    }) 
 }
 
 function ReturnViews() {
@@ -55,25 +64,24 @@ function ReturnViews() {
     console.log(data);
     var arr = new Array();
     data[0].rootTarget.views.map(obj => {
-        var button = "<button onclick=RenderPie('" + obj.view + "')>" + obj.view + "</button>";
+        var button = "<button onclick=RenderPie(ReturnFilteredView('" + obj.view + "','portfolio'))>" + obj.view + "</button>";
         document.getElementById("body").innerHTML += button;
     })
     return arr;
 }
 
-function ReturnFilteredView(target) {
+function ReturnFilteredView(view,target) {
     var data = returndata();
-    console.log("XX RETURNING VIEW:");
-    var arr = new Array();
-    var reformattedArray = data[0].rootTarget.views.filter((ob) => ob.view == target);
-    console.log(reformattedArray);
-    var result = reformattedArray[0].targets.map(obj => {
-        console.log("val: " + obj.marketValue);
-        return obj.marketValue + obj.target;
-    })
-    arr = result;
+    console.log("Searching for target views:"+view+" "+target);
+    var reformattedArray;
+    if(target == "portfolio"){
+        reformattedArray = data[0].rootTarget.views.filter((ob)=> ob.view == view);
+        return reformattedArray;
+    }
+    reformattedArray = data[0].rootTarget.views.filter((ob) => ob.view == view);
+    var result = reformattedArray[0].targets.filter((obj)  => obj.target == target);
     console.log(result);
-    return arr;
+    return result[0].views;
 }
 
 function NextView() {
@@ -90,7 +98,6 @@ function ClearCanvas() {
       for(var i = 0; i < d.length; i++){
             d[i].remove();
       }
-    
 }
 
 function build() {
